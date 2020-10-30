af975ecc645020556b3ae9f888d57c3ad190e4f9
Rommixcardano <73481274+Rommixcardano@users.noreply.github.com>
2020-10-30 09:32:49 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ func main() {
   fmt.Println("Hello World")
   fmt.Println("vedi che lo sto installando?")
   fmt.Println("---------------------------------")
-  fmt.Println("Prof non capisco nulla di questo linguaggio,")
+  fmt.Println("Hello Professor")
   fmt.Println("quindi aggiungerò solo questo messaggio :-)")
 }
